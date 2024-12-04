374912366a0e8969c3454dae9e0bcd6d83674178
GHOST <ghostdevbusiness@gmail.com>
2024-12-04 06:29:25 +0000

    feat: use one regex

--- 2024/src/days/03/two.aoc.ts
+++ 2024/src/days/03/two.aoc.ts
@@ -4,11 +4,7 @@ export async function run() {
     const input = await getInput(import.meta.dirname!, './input.txt');
 
     const result = input
-        .matchAll(/mul\((\d+),(\d+)\)/g)
-        .filter(({ index }) => {
-            const current = `do()${input.slice(0, index)}`;
-            return current.lastIndexOf("don't()") < current.lastIndexOf('do()');
-        })
+        .matchAll(/(?<=(?:(?:do\(\))|^)(?:(?!don't\(\)).)*)mul\((\d+),(\d+)\)/gs)
         .map(([, a, b]) => Number.parseInt(a) * Number.parseInt(b))
         .reduce((a, x) => a + x, 0);
 
