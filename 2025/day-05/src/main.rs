5d46033505866fc1b29e6d23facf23e11f8e8363
Willow (GHOST) <git@willow.sh>
2025-12-05 13:45:36 +0000

    feat: 2025 day five

---
+++ 2025/day-05/src/main.rs
@@ -0,0 +1,90 @@
+use std::ops::RangeInclusive;
+
+const INPUT: &str = include_str!("./input.txt");
+
+/// Flatten a vec of ranges into smaller non-overlapping vec
+fn simplify_ranges(mut ranges: Vec<RangeInclusive<i64>>) -> Vec<RangeInclusive<i64>> {
+    ranges.sort_by_key(|range| *range.start());
+
+    let mut new_ranges = Vec::<RangeInclusive<i64>>::new();
+    let mut current = ranges[0].to_owned();
+
+    for range in ranges.into_iter().skip(1) {
+        if current.end() < range.start() {
+            new_ranges.push(current);
+            current = range;
+            continue;
+        }
+
+        if current.end() >= range.start() {
+            let new_end = if current.end() > range.end() {
+                current.end()
+            } else {
+                range.end()
+            };
+
+            current = *current.start()..=*new_end;
+        }
+    }
+
+    new_ranges.push(current.to_owned());
+
+    return new_ranges;
+}
+
+fn main() {
+    let (fresh, available) = INPUT
+        .split_once("\n\n")
+        .expect("unable to find first/second half of list");
+
+    let fresh = fresh
+        .trim()
+        .lines()
+        .map(|line| {
+            let (start, end) = line
+                .split_once("-")
+                .expect(&format!("Unable to split fresh line: \"{}\"", line));
+
+            let start = i64::from_str_radix(start, 10).expect(&format!(
+                "Unable to parse fresh start to number \"{}\"",
+                start
+            ));
+
+            let end = i64::from_str_radix(end, 10)
+                .expect(&format!("Unable to parse fresh end to number \"{}\"", end));
+
+            start..=end
+        })
+        .collect::<Vec<_>>();
+
+    let fresh = simplify_ranges(fresh);
+
+    let available = available
+        .trim()
+        .lines()
+        .map(|line| {
+            i64::from_str_radix(line, 10).expect(&format!(
+                "Unable to parse available to number: \"{}\"",
+                line
+            ))
+        })
+        .collect::<Vec<_>>();
+
+    let mut total_fresh = 0;
+
+    for available in available.iter() {
+        for fresh_range in fresh.iter() {
+            if fresh_range.contains(&available) {
+                total_fresh += 1;
+                break;
+            }
+        }
+    }
+
+    let total_possible_fresh_ids = fresh
+        .iter()
+        .fold(0, |total, range| total + (range.end() - range.start() + 1));
+
+    println!("Part One: {}", total_fresh);
+    println!("Part Two: {}", total_possible_fresh_ids);
+}
